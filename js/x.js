c5f687e68ea71be18dd81b2e76ef9d2dd3bb7282
TheC <i@thec.me>
2015-06-05 12:10:47 +0800

    you can shoot ufo now

--- js/x.js
+++ js/x.js
@@ -1,6 +1,6 @@
 // global variables
 GAME_OVER = true;
-TIMESTAMP = 0;
+GAME_SCORE = 0;
 STAGE_WIDTH = 465;
 STAGE_HEIGHT = 465;
 
@@ -11,6 +11,13 @@ $.fn.active = function() {
 $.fn.deactive = function() {
 	$(this).removeClass('active');
 }
+// add remove method for array
+Array.prototype.remove = function(el) {
+	var index = this.indexOf(el);
+	if( index == -1 ) return false;
+	this.splice(index, 1);
+	return this;
+}
 // return the elapsed time from the last call
 function TIMER() {
 	var now = new Date().getTime(),
@@ -20,6 +27,51 @@ function TIMER() {
 }
 TIMER.last = new Date().getTime();
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// define warship
+function WARSHIP() {
+	var self = this;
+	self.x = 240;
+	self.dom = $('<div>').attr('id', 'warship').css('x', self.x - WARSHIP.width).appendTo(stage);
+	// bind keydown event
+	$(window).on('keydown', function(e) {
+		WARSHIP.key[e.keyCode] = true;
+		// fire
+		if( e.keyCode == 90 ) self.fire();
+	}).on('keyup', function(e) {
+		WARSHIP.key[e.keyCode] = false;
+	});
+}
+// move warship on keydown
+WARSHIP.prototype.loop = function() {
+	// map key
+	if( WARSHIP.key[37] ) {
+		this.x -= WARSHIP.speed; 
+		this.x = this.x > 0 ? this.x : 0;
+	}
+	if( WARSHIP.key[39] ) {
+		this.x += WARSHIP.speed; 
+		this.x = this.x < STAGE_WIDTH ? this.x : STAGE_WIDTH;
+	}
+	// move the ship
+	this.dom.css('left', this.x - WARSHIP.width);
+}
+// fire!
+WARSHIP.prototype.fire = function() {
+	var bullet = new BULLET(this.x);
+	BULLET.list.push(bullet);
+}
+// put the warship to its initial position
+WARSHIP.prototype.init = function() {
+	this.x = 240;
+	this.dom.css('x', this.x);
+}
+// static var
+WARSHIP.width = 96/2;
+WARSHIP.speed = 8;
+WARSHIP.key   = {37: false, 39: false, 90: false}; // save currently pressed key
+
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 // define UFO
@@ -27,25 +79,34 @@ function UFO(x) {
 	// private var
 	this.x = x;
 	this.y = -80;
-	this.dom = $('<div>').addClass('ufo').css({'left': this.x, 'top': this.y}).appendTo(stage);
+	// cache the left & right border of this ufo
+	this.left = this.x - UFO.width;
+	this.right = this.x + UFO.width;
+	this.dom = $('<div>').addClass('ufo').css({'left': this.x - UFO.width, 'top': this.y}).appendTo(stage);
 }
 // instance methods
+// main loop
 UFO.prototype.loop = function() {
 	this.y += UFO.speed;
-	// check if current ufo is out of border
-	if( this.y < STAGE_HEIGHT )
+	// check if current ufo has hit the earth
+	if( this.y < (STAGE_HEIGHT-120) )
 		this.dom.css('top', this.y);
-	else {
-		this.dom.remove();
-		UFO.list.shift(); // current ufo is always the first one in list
-		delete this;
-	}
+	else 
+		this.destroy();
+}
+// destroy
+UFO.prototype.destroy = function() {
+	this.dom.remove();
+	UFO.list.remove(this);
+	delete this;
 }
 // static vars
-UFO.speed = 5;    // speed
+UFO.width = 100/2;  // pixel width
+UFO.height= 80;
+UFO.speed = 2;    // speed
 UFO.list  = [];   // array which contains all current ufos
-UFO.cd    = 500;  // cooldown for new ufo
-UFO.rate  = .7;   // new ufo generate rate
+UFO.cd    = 1000;  // cooldown for new ufo
+UFO.rate  = .6;   // new ufo generate rate
 UFO.loop  = null; // the new ufo generating interval
 // static methods
 UFO.g     = function() { // generate new ufo
@@ -56,22 +117,65 @@ UFO.g     = function() { // generate new ufo
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
-// define warship
-function WARSHIP() {
-	this.x = 190;
-	this.dom = $('<div>').attr('id', 'warship').css('x', this.x).appendTo(stage);
+// define bullet
+function BULLET(x) {
+	// the x should be where warship is
+	this.x = x;
+	this.y = 385;
+	// cache the left & right border of this bullet
+	this.left = this.x - BULLET.width;
+	this.right = this.x + BULLET.width;
+	this.dom = $('<div>').addClass('bullet').css({'left': this.x - BULLET.width, 'top': this.y}).appendTo(stage);
 }
-// move warship on keydown
-WARSHIP.prototype.move = function(e) {
-	console.log(e);
-	// map key
+// move
+BULLET.prototype.loop = function() {
+	this.y -= BULLET.speed;
+	this.dom.css('top', this.y);
+	// over border check
+	if( this.y < -BULLET.height ) 
+		this.destroy();
+	// hit check
+	for( var i = 0; i < UFO.list.length; i++ ) {
+		var ufo = UFO.list[i];
+		// this condition may lose 6px, but it's much simpler & faster than precise condition
+		if( this.x > ufo.left && this.x < ufo.right && this.y < ufo.y + UFO.height ) {
+			ufo.destroy();
+			// boooom
+			new BOOM(this.x, this.y);
+		}
+	}
 }
-WARSHIP.prototype.init = function() {
-	this.x = 190;
-	this.dom.css('x', this.x);
+// destroy
+BULLET.prototype.destroy = function() {
+	this.dom.remove();
+	BULLET.list.shift(); // this bullet is always the first one in list
+	delete this;
 }
+// static vars
+BULLET.speed = 3;
+BULLET.width = 24/2;
+BULLET.height= 12;
+BULLET.list  = [];
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
+
+// define bom
+function BOOM(x, y) {
+	var self = this;
+	self.dom = $('<div>').addClass('boom').css({'left': x - BOOM.width, 'top': y - BOOM.height}).appendTo(stage);
+	// run self-destroy function
+	setTimeout(function() {
+		self.dom.remove();
+		delete self;
+	}, 1000);
+}
+// static
+BOOM.width = 120/2;
+BOOM.height = 120/2;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+// main logical
 
 // call this method only one time after the page loads completely
 // it caches elements which always has only one instance
@@ -83,6 +187,8 @@ function init() {
 	blink = $('#blink');
 	board = $('#gameover');
 	warship = new WARSHIP();
+	// 
+	start();
 }
 
 // start a game for user
@@ -103,6 +209,8 @@ function end() {
 	GAME_OVER = true;
 	// stop the generate loop
 	clearInterval(UFO.loop);
+	// clear ufos
+	UFO.list = [];
 	// show the score board
 	deactive_stage();
 }
@@ -114,9 +222,16 @@ function loop() {
 		return;
 	}
 	// move forward current elements
+	var i;
 	// never cache UFO.list.length here! because ufo.loop will modify the array async
-	for( var i = 0; i < UFO.list.length; i++ ) {
+	for( i = 0; i < UFO.list.length; i++ ) {
 		UFO.list[i].loop();
+	}
+	// warship move loop
+	warship.loop();
+	// move bullets
+	for( i = 0; i < BULLET.list.length; i++ ) {
+		BULLET.list[i].loop();
 	}
 	// do loop
 	requestAnimationFrame(loop);
